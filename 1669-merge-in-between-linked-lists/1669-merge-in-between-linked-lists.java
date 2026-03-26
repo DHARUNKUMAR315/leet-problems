e11034e6161f4129a2023449459accb7c68060c8
DHARUN KUMAR.R <dharunr412@gmail.com>
2026-03-26 16:33:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.java
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.java
@@ -1,29 +1,19 @@
 class Solution {
     public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
         ListNode prevA = list1;
-        // Move to node just before 'a'
         for (int i = 1; i < a; i++) {
             prevA = prevA.next;
         }
-
         ListNode afterB = prevA;
-        // Move to node just after 'b'
         for (int i = a; i <= b; i++) {
             afterB = afterB.next;
         }
-
-        // Connect prevA to list2
         prevA.next = list2;
-
-        // Find tail of list2
         ListNode tail = list2;
         while (tail.next != null) {
             tail = tail.next;
         }
-
-        // Connect tail of list2 to afterB
         tail.next = afterB.next;
-
         return list1;
     }
 }
