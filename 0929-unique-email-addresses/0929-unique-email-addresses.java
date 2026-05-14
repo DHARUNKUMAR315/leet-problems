b1ace776497796c1db856659bc21ace60c5d4567
DHARUN KUMAR.R <dharunr412@gmail.com>
2026-05-14 10:27:35 +0530

    Time: 12 ms (71.42%), Space: 46.5 MB (62.52%) - LeetHub

---
+++ 0929-unique-email-addresses/0929-unique-email-addresses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        Set<String> unique = new HashSet<>();
+        for(String s:emails)
+        {
+            String parts[]=s.split("@");
+            String local=parts[0];
+            String domain=parts[1];
+            int plusindex=local.indexOf('+');
+            if(plusindex!=-1)
+            {
+                local=local.substring(0,plusindex);
+            }
+            local=local.replace(".","");
+            String normalized = local + "@" + domain;
+            unique.add(normalized);
+        }
+         return unique.size();
+    }
+}
